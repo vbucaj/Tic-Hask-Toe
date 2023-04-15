c601515ed594cf85cc710234ffe74cab776cf6d0
Valmir Bucaj <bucajvalmir@gmail.com>
2023-04-15 13:45:05 +0000

    implemented init function

--- exampledir/EX4.hs
+++ exampledir/EX4.hs
@@ -15,9 +15,9 @@ countDown 0 = "Liftoff"
 
 -- Correct implementation of countDown function 
 
-countDown' :: Int -> String
-countDown' 0 = "Liftoff"
-countDown' n = if n >0 then countDown' (n-1) else "failed to launch"
+-- countDown' :: Int -> String
+-- countDown' 0 = "Liftoff"
+-- countDown' n = if n >0 then countDown' (n-1) else "failed to launch"
                 -- else "Failure to launch"
 
 
@@ -54,3 +54,30 @@ length' (x:xs) = 1 + length' xs
 elem' :: Eq a => a -> [a] -> Bool 
 elem' q (x:xs) = q==x || elem' q xs
 elem' _ [] = False 
+
+
+-- elem'' :: Eq a => a->[a]->Bool
+-- elem'' q (x:xs)
+--     | q==x = True
+--     | elem'' q xs 
+
+
+-- implementation of zip
+
+zip' :: [a]->[b]->[(a,b)]
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+zip' [] _ = []
+zip' _ [] = []
+
+-- implementation of last
+last' :: [a]->a 
+last' [] = error "empty list"
+last' [x] = x
+last' (_:xs) = last' xs
+
+-- implementation of init 
+
+init' :: [a] -> [a] 
+init' [x] = []
+init' (x:xs) = x : init' xs
+init' [] = error "empty list"
